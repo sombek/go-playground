8cbe75a7aecb1ffecadcac6ec15fd17e80411471
Abdullah Hashim <ahashim@rega.gov.sa>
2024-08-02 18:06:14 +0300

    mocking: finish the exercise

--- mocking/main.go
+++ mocking/main.go
@@ -7,39 +7,55 @@ import (
 	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := 3; i > 0; i-- {
-		_, err := fmt.Fprintln(out, i)
-		if err != nil {
-			return
-		}
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
-	_, err := fmt.Fprint(out, "Go!")
-	if err != nil {
-		return
-	}
+	fmt.Fprint(out, finalWord)
 }
 
-type SpySleeper struct {
-	Calls int
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
